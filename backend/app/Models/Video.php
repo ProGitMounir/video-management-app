e8c969ce82cbf8440025a1c855cdfdc262548645
ProGitMounir <pro.mailmounir@gmail.com>
2025-03-20 17:06:52 +0000

    Création du modèle "Video" pour interagir avec la table videos

---
+++ backend/app/Models/Video.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'video_name',
+        'video_url',
+        'video_duration',
+        'video_aspect_ratio',
+        'order_position',
+    ];
+}
